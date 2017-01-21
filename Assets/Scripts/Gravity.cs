a9827c53b28b83ff1f642e7d6d345d60c084a449
Ryan Saul <seccom@gmail.com>
2017-01-20 19:21:54 -0800

    got basic gravity and player controls to work

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+	private Rigidbody2D _playerController;
+
+	// Use this for initialization
+	void Start () {
+		_playerController = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 gravityWell = new Vector2 (0, 0);
+		Vector2 playerPos = new Vector2 (_playerController.transform.position.x, _playerController.transform.position.y);
+		Vector2 gravity = gravityWell - playerPos;
+		_playerController.AddForce (gravity);
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	private Rigidbody2D _playerController;
+	public float thrustVelocity = 5.0f;
+	public float turnSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		_playerController = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (0, 0, - Input.GetAxis ("Horizontal") * turnSpeed);
+
+		if (Input.GetAxisRaw ("Jump") != 0) {
+			_playerController.AddRelativeForce (new Vector2 (0, thrustVelocity));
+		}
+	}
+}
