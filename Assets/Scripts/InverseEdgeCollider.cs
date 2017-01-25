dc152b1e5124956201bb19a92955583b5584bdf1
Dylan Gulick <dgulick@jamasoftware.com>
2017-01-25 12:33:10 -0800

    Upped InverseEdgeCollider edges to hopefully avoid allowing ships to sneak through

--- Assets/Scripts/InverseEdgeCollider.cs
+++ Assets/Scripts/InverseEdgeCollider.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class InverseEdgeCollider : MonoBehaviour
 {
 
-    public int NumEdges = 1000;
+    public int NumEdges = 3000;
 
     // Use this for initialization
     void Start()
